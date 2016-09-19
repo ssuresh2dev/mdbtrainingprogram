e3b5253fee8aa7e5c4713a51428e75a8fa20008c
eliot1019 <eliot1019@gmail.com>
2016-09-19 00:15:56 -0700

    Initial Commit

---
+++ MDB Project 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDB Project 1
+//
+//  Created by Eliot Han on 9/19/16.
+//  Copyright © 2016 Eliot Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
