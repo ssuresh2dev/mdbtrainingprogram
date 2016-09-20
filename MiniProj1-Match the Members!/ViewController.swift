f65747adcb73a0bf5b7175db290590d3253c19ce
Vidya Ravikumar <vravikumar@berkeley.edu>
2016-09-20 14:54:49 -0700

    Initial Commit

---
+++ MiniProj1-Match the Members!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniProj1-Match the Members!
+//
+//  Created by Vidya Ravikumar on 9/20/16.
+//  Copyright © 2016 Vidya Ravikumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
