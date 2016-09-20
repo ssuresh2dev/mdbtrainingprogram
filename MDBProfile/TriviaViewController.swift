cd81603c4f5593e507b2cf862a2083521fd589ba
Ben Goldberg <benisgold@gmail.com>
2016-09-20 13:41:59 -0700

    test

--- MDBProfile/TriviaViewController.swift
+++ MDBProfile/TriviaViewController.swift
@@ -108,6 +108,7 @@ class TriviaViewController: UIViewController {
     }
 
     override func viewDidLoad() {
+        picture.contentMode = .scaleAspectFit
         pickImage()
         makeButtons()
     }
