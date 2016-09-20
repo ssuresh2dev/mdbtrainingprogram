8695d44e25d3e15587d9a956464060aab4fd58e6
Ben Goldberg <bgoldberg@berkeley.edu>
2016-09-19 23:03:08 -0700

    LOTS OF PROGRESS!

--- MDBProfile/TriviaViewController.swift
+++ MDBProfile/TriviaViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class TriviaViewController: UIViewController {
     
+    var name = ""
     var picked: [String] = []
-    let names = ["Aayush Tyagi", "Abhinav Koppu", "Abhishek Mangla", "Akkshay Khoslaa", "Alice Wang", "Ali Shelton", "Ally Koo", "Andy Wang", "Aneesh Jindal", "Anisha Salunkhe", "Ankur Mahesh", "Aparna Krishnan", "Ashwin Vaidyanathan", "Ben Goldberg", "Billy Lu", "Candice Ye", "Christine Munar", "Cody Hsieh", "Connor Killion", "Edward Liu", "Eliot Han", "Emaan Hariri", "Jared Gutierrez", "Jeffrey Zhang", "Jessica Chen", "Jessica Cherny", "Jessica Ji", "Justin Kim", "Katharine Jiang", "Kedar Thakkar", "Kenneth Steele", "Kevin Jiang", "Krishnan Rajiyah", "Kristin Ho", "Leon Kwak", "Lisa Lee", "Mansi Shah", "Mohit Katyal", "Mudit Mittal", "Peter Schafhalter", "Richard Chen", "Richard Hu", "Riley Edmunds", "Rochelle Shen", "Rohan Narayan", "Sahil Lamba", "Sameer Suresh", "Sayan Paul", "Shaan Appel", "Sharie Wang", "Shreya Reddy", "Shubham Goenka", "Sirjan Kafle", "Sona Jeswani", "Tarun Khasnavis", "Victor Sun", "Vidya Ravikumar", "Virindh Borra", "Wilbur Shi", "Young Lin"]
+    var images: [String] = []
+    var gender = arc4random_uniform(2)
+    let guyNames = ["Aayush Tyagi", "Abhinav Koppu", "Abhishek Mangla", "Akkshay Khoslaa", "Ali Shelton", "Andy Wang", "Aneesh Jindal", "Ankur Mahesh", "Ashwin Vaidyanathan", "Ben Goldberg", "Billy Lu", "Cody Hsieh", "Connor Killion", "Edward Liu", "Eliot Han", "Emaan Hariri", "Jared Guiterrez", "Jeffrey Zhang", "Justin Kim", "Kedar Thakkar", "Kenneth Steele", "Kevin Jiang", "Krishnan Rajiyah", "Leon Kwak", "Mohit Katyal", "Mudit Mittal", "Peter Schafhalter", "Richard Chen", "Richard Hu", "Riley Edmunds", "Rohan Narayan", "Sahil Lamba", "Sameer Suresh", "Sayan Paul", "Shaan Appel", "Shubham Goenka", "Sirjan Kafle", "Tarun Khasnavis", "Victor Sun", "Virindh Borra", "Wilbur Shi", "Young Lin"]
+    
+    let girlNames = ["Alice Wang", "Ally Koo", "Anisha Salunkhe", "Aparna Krishnan", "Candice Ye", "Christine Munar", "Jessica Chen", "Jessica Cherny", "Jessica Ji", "Katharine Jiang", "Kristin Ho", "Lisa Lee", "Mansi Shah", "Rochelle Shen", "Sharie Wang", "Shreya Reddy", "Sona Jeswani", "Vidya Ravikumar"]
+
 
     @IBOutlet weak var picture: UIImageView!
     @IBOutlet weak var startStop: UIButton!
@@ -20,58 +26,70 @@ class TriviaViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
-    func pickName(button: UIButton) {
-        let newName = names[Int(arc4random_uniform(60))]
-        if picked.contains(newName) {
-            pickName(button: button)
+    @IBAction func next(_ sender: UIButton) {
+        pickImage()
+        makeButtons()
+    }
+    
+    func pickImage() {
+        if gender == 1 {
+            name = guyNames[Int(arc4random_uniform(42))]
+            while images.contains(name) {
+                name = guyNames[Int(arc4random_uniform(42))]
+            }
+            images += [name]
+            let imageName = String(name.characters.filter {$0 != " "})
+            picture.image = UIImage(named: imageName)
         } else {
-            picked += [newName]
-            button.setTitle(newName, for: .normal)
+            name = girlNames[Int(arc4random_uniform(18))]
+            while images.contains(name) {
+                name = girlNames[Int(arc4random_uniform(18))]
+            }
+            images += [name]
+            let imageName = String(name.characters.filter {$0 != " "})
+            picture.image = UIImage(named: imageName)
         }
     }
     
-    @IBAction func answerChosen(_ sender: UIButton) {
-        pickName(button: button0)
-        pickName(button: button1)
-        pickName(button: button2)
-        pickName(button: button3)
-        let correctButtonIndex = arc4random_uniform(4)
+    func makeButtons() {
+        var buttonArray = [button0, button1, button2, button3]
+        let correctButtonIndex = Int(arc4random_uniform(4))
+        buttonArray.remove(at: correctButtonIndex)
+        
         switch correctButtonIndex {
         case 0:
-            let imageName = String(describing: button0.titleLabel).trimmingCharacters(in: .whitespaces)
-            picture.image = UIImage(named: imageName)
+            button0.setTitle(name, for: .normal)
         case 1:
-            let imageName = String(describing: button1.titleLabel).trimmingCharacters(in: .whitespaces)
-            picture.image = UIImage(named: imageName)
+            button1.setTitle(name, for: .normal)
         case 2:
-            let imageName = String(describing: button2.titleLabel).trimmingCharacters(in: .whitespaces)
-            picture.image = UIImage(named: imageName)
+            button2.setTitle(name, for: .normal)
         case 3:
-            let imageName = String(describing: button3.titleLabel).trimmingCharacters(in: .whitespaces)
-            picture.image = UIImage(named: imageName)
+            button3.setTitle(name, for: .normal)
         default:
             break
         }
+        
+        var randomName = ""
+        for button in buttonArray {
+            if gender == 1 {
+                randomName = guyNames[Int(arc4random_uniform(42))]
+                while randomName == name || picked.contains(randomName) {
+                    randomName = guyNames[Int(arc4random_uniform(42))]
+                }
+            } else {
+                randomName = girlNames[Int(arc4random_uniform(18))]
+                while randomName == name || picked.contains(randomName) {
+                    randomName = girlNames[Int(arc4random_uniform(18))]
+                }
+            }
+            picked += [randomName]
+            button!.setTitle(randomName, for: .normal)
+        }
+        gender = arc4random_uniform(2)
     }
-    
-    let guyNames = ["Aayush Tyagi", "Abhinav Koppu", "Abhishek Mangla", "Akkshay Khoslaa", "Ali Shelton", "Andy Wang", "Aneesh Jindal", "Ankur Mahesh", "Ashwin Vaidyanathan", "Ben Goldberg", "Billy Lu", "Cody Hsieh", "Connor Killion", "Edward Liu", "Eliot Han", "Emaan Hariri", "Jared Guiterrez", "Jeffrey Zhang", "Justin Kim", "Kedar Thakkar", "Kenneth Steele", "Kevin Jiang", "Krishnan Rajiyah", "Leon Kwak", "Mohit Katyal", "Mudit Mittal", "Peter Schafhalter", "Richard Chen", "Richard Hu", "Riley Edmunds", "Rohan Narayan", "Sahil Lamba", "Sameer Suresh", "Sayan Paul", "Shaan Appel", "Shubham Goenka", "Sirjan Kafle", "Tarun Khasnavis", "Victor Sun", "Virindh Borra", "Wilbur Shi", "Young Lin"]
-    
-    let girlNames = ["Alice Wang", "Ally Koo", "Anisha Salunkhe", "Aparna Krishnan", "Candice Ye", "Christine Munar", "Jessica Chen", "Jessica Cherny", "Jessica Ji", "Katharine Jiang", "Kristin Ho", "Lisa Lee", "Mansi Shah", "Rochelle Shen", "Sharie Wang", "Shreya Reddy", "Sona Jeswani", "Vidya Ravikumar"]
-    
+
     override func viewDidLoad() {
-        pickName(button: button0)
-        pickName(button: button1)
-        pickName(button: button2)
-        pickName(button: button3)
-        let correctButton = arc4random_uniform(4)
-        switch correctButton {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        default:
-            break
-        }
+        pickImage()
+        makeButtons()
     }
-    
 }
