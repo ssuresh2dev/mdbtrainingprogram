22ca7b5c54420ccbcb3afd8d47e5d2b0ae66da10
Ben Goldberg <benisgold@gmail.com>
2016-09-22 21:13:02 -0700

    more stats

--- MDBProfile/StatisticViewController.swift
+++ MDBProfile/StatisticViewController.swift
@@ -2,34 +2,40 @@
 //  StatisticViewController.swift
 //  MDBProfile
 //
-//  Created by Rochelle on 9/22/16.
+//  Created by Ben Goldberg on 9/22/16.
 //  Copyright © 2016 Rochelle. All rights reserved.
 //
 
 import UIKit
 
 class StatisticViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var topScore = 0
+    var mostRecent: [String]!
+    var score: Int!
+    var name: String!
+    var picked: [String]!
+    var girlsImages: [String]!
+    var guysImages: [String]!
+    var gender: UInt32!
+    var seconds: Int!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let trivia = segue.destination as! TriviaViewController
+        trivia.topScore = topScore
+        trivia.score = score
+        trivia.mostRecent = mostRecent
+        trivia.name = name
+        trivia.picked = picked
+        trivia.girlsImages = girlsImages
+        trivia.guysImages = guysImages
+        trivia.gender = gender
+        trivia.seconds = seconds
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        if score > topScore {
+            topScore = score
+        }
     }
-    */
-
 }
