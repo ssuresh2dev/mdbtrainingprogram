573411766af6c59c6c31f8b52784744b4b9482fd
Sayan Paul <sayan.paul.us@gmail.com>
2016-09-28 16:55:31 -0700

    Get pokemon to show in reyclerview using grid

--- app/src/main/java/mdb/project2/pokedex/MainActivity.java
+++ app/src/main/java/mdb/project2/pokedex/MainActivity.java
@@ -2,12 +2,26 @@ package mdb.project2.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Pokedex pokedex = new Pokedex();
+    private ArrayList<Pokedex.Pokemon> pokeList = pokedex.getPokemon();
+    private PokemonAdapter pokemonAdapter;
+
+    private RecyclerView pokemonListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pokemonListView = (RecyclerView) findViewById(R.id.pokemon_list);
+        pokemonListView.setLayoutManager(new GridLayoutManager(this, 2));
+        PokemonAdapter pokemonAdapter = new PokemonAdapter(getApplicationContext(), pokeList);
+        pokemonListView.setAdapter(pokemonAdapter);
     }
 }
