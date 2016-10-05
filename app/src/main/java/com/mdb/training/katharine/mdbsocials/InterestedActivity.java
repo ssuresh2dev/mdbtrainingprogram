81773703d9aec9b6faf8526050043cef839dd5ea
victorsun123 <victor7sun@gmail.com>
2016-10-05 12:06:34 -0700

    merge?

--- app/src/main/java/com/mdb/training/katharine/mdbsocials/InterestedActivity.java
+++ app/src/main/java/com/mdb/training/katharine/mdbsocials/InterestedActivity.java
@@ -1,7 +1,11 @@
 package com.mdb.training.katharine.mdbsocials;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class InterestedActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class InterestedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interested);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<User> interestedMembers = new ArrayList<User>();
+        //Todo: use firebase to get interested users
+
+        InterestedAdapter interestedAdapter = new InterestedAdapter(getApplicationContext(), interestedMembers);
+
+        recyclerView.setAdapter(interestedAdapter);
     }
 }
