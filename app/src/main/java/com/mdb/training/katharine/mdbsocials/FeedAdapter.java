e8ae5f1903c272745daf78ac5cf05940990d91a9
victorsun123 <victor7sun@gmail.com>
2016-10-05 02:27:20 -0700

    test

--- app/src/main/java/com/mdb/training/katharine/mdbsocials/FeedAdapter.java
+++ app/src/main/java/com/mdb/training/katharine/mdbsocials/FeedAdapter.java
@@ -1,22 +1,9 @@
 package com.mdb.training.katharine.mdbsocials;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
-
 /**
  * Created by KJ on 10/4/16.
  */
-
+/**
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.CustomViewHolder> {
 
     private Context context;
@@ -37,7 +24,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.CustomViewHold
     a ViewHolder, and that ViewHolder holder these 3 views in it (hence "view holder").
     This function returns a single ViewHolder; it is called once for every row.
     */
-    @Override
+    /**@Override
     public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 //        This "inflates" the views, using the layout R.layout.row_view
         View view;
@@ -50,7 +37,9 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.CustomViewHold
     example, "holder.imageView" we are accessing the imageView for that row and setting the
     ImageResource to be the corresponding image for that subject.
      */
-    @Override
+
+
+    /**@Override
     public void onBindViewHolder(CustomViewHolder holder, int position) {
         SocialsList.Social pokemon = socials.get(position);
         holder.pokemonNameTextView.setText(pokemon.name);
@@ -75,7 +64,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.CustomViewHold
                     /*Get adapter position is getting the number of the row that was clicked,
                     starting at 0
                     */
-                    SocialsList.Social pokemon = socials.get(getAdapterPosition());
+                    /**SocialsList.Social pokemon = socials.get(getAdapterPosition());
                     Intent intent = new Intent(context, DetailsActivity.class);
                     intent.putExtra("name", pokemon.name);
                     intent.putExtra("number", pokemon.number);
@@ -92,4 +81,4 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.CustomViewHold
             });
         }
     }
-}
+}**/
