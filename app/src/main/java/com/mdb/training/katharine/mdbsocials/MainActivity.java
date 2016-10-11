a1ecb3bc77e02e2e182a80b2e56d0c970d25ee5d
victorsun123 <victor7sun@gmail.com>
2016-10-10 18:11:43 -0700

    update

--- app/src/main/java/com/mdb/training/katharine/mdbsocials/MainActivity.java
+++ app/src/main/java/com/mdb/training/katharine/mdbsocials/MainActivity.java
@@ -1,10 +1,9 @@
 package com.mdb.training.katharine.mdbsocials;
 
 import android.content.Intent;
-import android.nfc.Tag;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -89,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (!task.isSuccessful()) {
+                    Log.d("testing", task.getException().getMessage());
+                    task.getException().printStackTrace();
                     Toast.makeText(MainActivity.this, "Sign in problem", Toast.LENGTH_LONG).show();
                 }
             }
