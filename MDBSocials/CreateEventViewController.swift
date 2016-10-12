fa08707b88971344f1228ec0864112d4b5786dae
eliot1019 <eliot1019@gmail.com>
2016-10-11 18:49:56 -0700

    Post Button Added and unwind segue created.

--- MDBSocials/CreateEventViewController.swift
+++ MDBSocials/CreateEventViewController.swift
@@ -9,27 +9,30 @@
 import UIKit
 
 class CreateEventViewController: UIViewController {
-
+    
+    var eventTitleLabel: UILabel!
+    var eventDescriptonTextField: UITextView!
+    var imageView: UIImageView!
+    var eventDateLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupUI(){
+        let postEventButton = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(CreateEventViewController.postButtonClicked))
+        self.navigationItem.rightBarButtonItem = postEventButton
     }
-    */
-
+    
+    func postButtonClicked(){
+        
+        performSegue(withIdentifier: "unwindToFeedVC", sender: self)
+    }
 }
