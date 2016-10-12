9696c48e0a03c9091480e986c5d06699c33e5f6d
eliot1019 <eliot1019@gmail.com>
2016-10-12 00:45:17 -0700

    Added UIImagePicker

--- MDBSocials/CreateEventViewController.swift
+++ MDBSocials/CreateEventViewController.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class CreateEventViewController: UIViewController {
+class CreateEventViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var eventTitleField: UITextField!
     var eventDescriptonTextField: UITextField!
     var imageView: UIImageView!
-    var eventDateLabel: UILabel!
-    
+    var eventDateTextField: UITextField!
+    var imagePicker = UIImagePickerController()
+    var eventImageView: UIImageView! = UIImageView(image: UIImage(named: "emptyimage"))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        imagePicker.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,13 +42,44 @@ class CreateEventViewController: UIViewController {
         eventDescriptonTextField.placeholder = "Describe your event."
         view.addSubview(eventDescriptonTextField)
         
-        //photopicker cocoapod here
+
+        eventDateTextField = UITextField(frame: CGRect(x: 20, y: eventDescriptonTextField.frame.maxY + 25, width: view.frame.width-40, height: 50))
+        eventDateTextField.placeholder = "Date"
+        view.addSubview(eventDateTextField)
         
+        eventImageView.frame = CGRect(x: 20, y: eventDateTextField.frame.maxY + 20, width: view.frame.width - 40, height: 320)
+        view.addSubview(eventImageView!)
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action: #selector(CreateEventViewController.imageTapped))
+        eventImageView.isUserInteractionEnabled = true
+        eventImageView.addGestureRecognizer(tapGestureRecognizer)
         
     }
+    func imageTapped(){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum){
+            print("Button capture")
+            
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            eventImageView.image = image
+        } else{
+            print("Something went wrong")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+
     
     func postButtonClicked(){
-        
         performSegue(withIdentifier: "unwindToFeedVC", sender: self)
     }
 }
