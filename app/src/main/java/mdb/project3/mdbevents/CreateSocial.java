5caea57af69b81784286b45eab159763d0d86a10
Sayan Paul <sayan.paul.us@gmail.com>
2016-10-11 17:44:40 -0700

    Allow user to upload image to event

--- app/src/main/java/mdb/project3/mdbevents/CreateSocial.java
+++ app/src/main/java/mdb/project3/mdbevents/CreateSocial.java
@@ -1,6 +1,11 @@
 package mdb.project3.mdbevents;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,22 +15,26 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
 import java.util.Calendar;
 
 public class CreateSocial extends AppCompatActivity implements View.OnClickListener {
 
+    private final int PICK_IMAGE_REQUEST = 1;
+
     private DatePickerDialog.OnDateSetListener[] dateSetListeners = new DatePickerDialog.OnDateSetListener[3];
-    private EditText[] dates;
-    private EditText eventName, eventDescription;
-    private ImageButton eventPicture;
+    private EditText dates[], socialName, socialDescription;
+    private TextView createSocialTitle;
+    private ImageButton socialPictureView;
     private Calendar myCalendar = Calendar.getInstance();
     private boolean updatedPicture = false;
     private AppCompatButton createSocialButton;
+    private Bitmap socialImage;
 
     private DatabaseReference dbRef;
 
@@ -47,14 +56,18 @@ public class CreateSocial extends AppCompatActivity implements View.OnClickListe
                 (EditText) findViewById(R.id.third_date)
         };
 
-        eventName = (EditText) findViewById(R.id.name_of_event);
-        eventDescription = (EditText) findViewById(R.id.event_description);
-        eventPicture = (ImageButton) findViewById(R.id.event_image);
-        createSocialButton = (AppCompatButton) findViewById(R.id.create_event_button);
+        createSocialTitle = (TextView) findViewById(R.id.new_social_title);
+        socialName = (EditText) findViewById(R.id.name_of_social);
+        socialDescription = (EditText) findViewById(R.id.social_description);
+        socialPictureView = (ImageButton) findViewById(R.id.social_image);
+        createSocialButton = (AppCompatButton) findViewById(R.id.create_social_button);
+
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/cornerstone.ttf");
+        createSocialTitle.setTypeface(customFont);
 
         for (EditText e : dates)
             e.setOnClickListener(this);
-        eventPicture.setOnClickListener(this);
+        socialPictureView.setOnClickListener(this);
         createSocialButton.setOnClickListener(this);
     }
 
@@ -79,8 +92,8 @@ public class CreateSocial extends AppCompatActivity implements View.OnClickListe
     }
 
     private boolean validate() {
-        boolean emptyFields = TextUtils.isEmpty(eventName.getText().toString()) ||
-                TextUtils.isEmpty(eventDescription.getText().toString()) ||
+        boolean emptyFields = TextUtils.isEmpty(socialName.getText().toString()) ||
+                TextUtils.isEmpty(socialDescription.getText().toString()) ||
                 (TextUtils.isEmpty(dates[0].getText().toString()) &&
                         TextUtils.isEmpty(dates[1].getText().toString()) &&
                         TextUtils.isEmpty(dates[2].getText().toString()));
@@ -121,9 +134,40 @@ public class CreateSocial extends AppCompatActivity implements View.OnClickListe
                         myCalendar.get(Calendar.DAY_OF_MONTH))
                         .show();
                 break;
-            case R.id.create_event_button:
+            case R.id.social_image:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
+                break;
+            case R.id.create_social_button:
                 updateDatabase();
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST &&
+                resultCode == RESULT_OK &&
+                data != null &&
+                data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                socialImage = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                if (socialImage.getWidth() > socialPictureView.getWidth()) {
+                    int newHeight = (socialImage.getHeight() * socialPictureView.getWidth()) / socialImage.getWidth();
+                    socialPictureView.setImageBitmap(
+                            Bitmap.createScaledBitmap(socialImage, socialPictureView.getWidth(), newHeight, false));
+                } else {
+                    socialPictureView.setImageBitmap(socialImage);
+                }
+
+                updatedPicture = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
