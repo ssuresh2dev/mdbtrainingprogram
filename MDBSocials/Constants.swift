31fae57f92d998d9625d9f569efc4c4de0ea6a44
eliot1019 <eliot1019@gmail.com>
2016-10-11 19:16:46 -0700

    Setup basic UI in create event VC. Added a constants file to access common properties like color throughout the app. Planning on importing a cocoapod for imagepicker in next commit.

---
+++ MDBSocials/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  MDBSocials
+//
+//  Created by Eliot Han on 10/11/16.
+//  Copyright © 2016 Eliot Han. All rights reserved.
+//
+
+import UIKit
+
+struct Constants{
+    struct Colors{
+        static let purpleColor = UIColor(red:0.82, green:0.52, blue:0.99, alpha:1.0)
+        
+
+    }
+   
+}
--- MDBSocials/CreateEventViewController.swift
+++ MDBSocials/CreateEventViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class CreateEventViewController: UIViewController {
     
-    var eventTitleLabel: UILabel!
-    var eventDescriptonTextField: UITextView!
+    var eventTitleField: UITextField!
+    var eventDescriptonTextField: UITextField!
     var imageView: UIImageView!
     var eventDateLabel: UILabel!
     
@@ -27,8 +27,22 @@ class CreateEventViewController: UIViewController {
     }
     
     func setupUI(){
+        view.backgroundColor = Constants.Colors.purpleColor
+        
         let postEventButton = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(CreateEventViewController.postButtonClicked))
         self.navigationItem.rightBarButtonItem = postEventButton
+        
+        eventTitleField = UITextField(frame: CGRect(x: 20, y: (self.navigationController?.navigationBar.frame.height)! + 20, width: view.frame.width - 20, height: 50))
+        eventTitleField.placeholder = "What is the title of your event?"
+        view.addSubview(eventTitleField)
+        
+        eventDescriptonTextField = UITextField(frame: CGRect(x: 20, y: eventTitleField.frame.maxY + 25, width: view.frame.width - 20, height: 50))
+        eventDescriptonTextField.placeholder = "Describe your event."
+        view.addSubview(eventDescriptonTextField)
+        
+        //photopicker cocoapod here
+        
+        
     }
     
     func postButtonClicked(){
