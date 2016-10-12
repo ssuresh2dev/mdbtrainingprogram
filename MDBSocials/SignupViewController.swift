182e44a5716d878d6c668b4389cc6b2f3427eed5
Rochelle Shen <rochelleshen@berkeley.edu>
2016-10-12 00:10:45 -0700

    constants...

--- MDBSocials/SignupViewController.swift
+++ MDBSocials/SignupViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class SignupViewController: UIViewController {
     var firstName: UILabel!
@@ -25,15 +26,15 @@ class SignupViewController: UIViewController {
     var emailDisplay: UILabel!
     var emailField: UITextField!
     
+    var registerBox: UIButton!
+    
     var password: String = ""
     var username: String = ""
     var email: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        display()
-//        let moreDisplay = RectangleSignUp(frame: CGRect(x: 30, y: view.frame.height/4, width: view.frame.width - 60, height: view.frame.height/3), firstName: firstName, lastName: lastName, firstNameField: firstNameField, lastNameField: lastNameField, usernameDisplay: usernameDisplay, usernameField: usernameField, passwordDisplay: passwordDisplay, passwordField: passwordField, emailDisplay: emailDisplay, emailField: emailField)
-//        view.addSubview(moreDisplay)
+        setUpUI()
         // Do any additional setup after loading the view.
     }
     
@@ -43,38 +44,62 @@ class SignupViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        //        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: <#T##FIRAuthResultCallback?##FIRAuthResultCallback?##(FIRUser?, Error?) -> Void#>)
+
     }
     
-    func display() {
-        firstName = UILabel(frame: CGRect(x: 10, y: 15, width: 200, height: 30))
+    func pressRegister(_sender: UIButton) {
+        guard let emailAdd = emailField.text, let pass = passwordField.text else { return }
+        FIRAuth.auth()?.createUser(withEmail: emailAdd, password: pass) {(user, error) in
+            if let error = error {
+                print(error)
+                return
+            } else {
+                print("User signed in!")
+            }
+        }
+    }
+    
+    func setUpUI() {
+        firstName = UILabel(frame: CGRect(x: 48, y: 162, width: 75, height: 19))
         firstName.text = "First Name"
         view.addSubview(firstName)
-        lastName = UILabel(frame: CGRect(x: view.frame.width + 20, y: 15, width: 200, height: 30))
+        lastName = UILabel(frame: CGRect(x: 205, y: 163, width: 73, height: 19))
         lastName.text = "Last Name"
         view.addSubview(lastName)
         
-        firstNameField = UITextField(frame: CGRect(x: 10, y: 47, width: 300, height: 60))
+        firstNameField = UITextField(frame: CGRect(x: 47, y: 181, width: 122, height: 33))
         firstNameField.placeholder = "lemme see"
         view.addSubview(firstNameField)
-        lastNameField = UITextField(frame: CGRect(x: view.frame.width + 20, y: 47, width: 300, height: 60))
+        lastNameField = UITextField(frame: CGRect(x: 204, y: 181, width: 122, height: 33))
         lastNameField.placeholder = "YAS"
         view.addSubview(lastNameField)
         
-        usernameDisplay = UILabel(frame: CGRect(x: 10, y: 117, width: view.frame.width - 40, height: 30))
+        usernameDisplay = UILabel(frame: CGRect(x: 45.5, y: 227, width: 70, height: 19))
         usernameDisplay.text = "Username"
         view.addSubview(usernameDisplay)
-        usernameField = UITextField(frame: CGRect(x: 10, y: 153, width: view.frame.width-40, height: 60))
+        usernameField = UITextField(frame: CGRect(x: 46, y: 246, width: 273, height: 3))
         usernameField.placeholder = "OK"
         view.addSubview(usernameField)
         
-        emailDisplay = UILabel(frame: CGRect(x: 10, y: 228, width: view.frame.width-40, height: 30))
+        passwordDisplay = UILabel(frame: CGRect(x: 46, y: 299, width: 66, height: 19))
+        passwordDisplay.text = "Password"
+        view.addSubview(passwordDisplay)
+        passwordField = UITextField(frame: CGRect(x: 46, y: 318, width: 279, height: 33))
+        passwordField.placeholder = "PASS"
+        view.addSubview(passwordField)
+        
+        emailDisplay = UILabel(frame: CGRect(x: 48, y: 373, width: 38, height: 19))
         emailDisplay.text = "Email"
         view.addSubview(emailDisplay)
-        
-        emailField = UITextField(frame: CGRect(x: 10, y: 263, width: view.frame.width-40, height: 60))
+        emailField = UITextField(frame: CGRect(x: 47, y: 389, width: 279, height: 33))
         emailField.placeholder = "HAAHHA"
         view.addSubview(emailField)
+        
+        registerBox = UIButton(frame: CGRect(x: 106, y: 456, width: 165, height: 33))
+        registerBox.setTitle("Register", for: .normal)
+        registerBox.backgroundColor = UIColor(red:0.60, green:0.83, blue:0.82, alpha:1.0)
+        registerBox.setTitleColor(UIColor.white, for: .normal)
+        view.addSubview(registerBox)
     }
     
     
@@ -87,74 +112,4 @@ class SignupViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
-    
-    class RectangleSignUp: UIView {
-        var firstName: UILabel!
-        var lastName: UILabel!
-        
-        var firstNameField: UITextField!
-        var lastNameField: UITextField!
-        
-        var usernameDisplay: UILabel!
-        var usernameField: UITextField!
-        
-        var passwordDisplay: UILabel!
-        var passwordField: UITextField!
-        
-        var emailDisplay: UILabel!
-        var emailField: UITextField!
-        
-        init(frame: CGRect, firstName: UILabel, lastName: UILabel, firstNameField: UITextField, lastNameField: UITextField, usernameDisplay: UILabel, usernameField: UITextField, passwordDisplay: UILabel, passwordField: UITextField, emailDisplay: UILabel, emailField: UITextField) {
-            super.init(frame: frame)
-            self.firstNameField = firstNameField
-            self.firstName = firstName
-            self.lastNameField = lastNameField
-            self.lastName = lastName
-            self.usernameDisplay = usernameDisplay
-            self.usernameField = usernameField
-            self.passwordDisplay = passwordDisplay
-            self.passwordField = passwordField
-            self.emailField = emailField
-            self.emailDisplay = emailDisplay
-            screenDisplay()
-            
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        func screenDisplay() {
-            firstName = UILabel(frame: CGRect(x: 10, y: 15, width: 200, height: 30))
-            firstName.text = "First Name"
-            addSubview(firstName)
-            lastName = UILabel(frame: CGRect(x: frame.width + 20, y: 15, width: 200, height: 30))
-            lastName.text = "Last Name"
-            addSubview(lastName)
-            
-            firstNameField = UITextField(frame: CGRect(x: 10, y: 47, width: 300, height: 60))
-            firstNameField.placeholder = "lemme see"
-            addSubview(firstNameField)
-            lastNameField = UITextField(frame: CGRect(x: frame.width + 20, y: 47, width: 300, height: 60))
-            lastNameField.placeholder = "YAS"
-            addSubview(lastNameField)
-            
-            usernameDisplay = UILabel(frame: CGRect(x: 10, y: 117, width: frame.width - 40, height: 30))
-            usernameDisplay.text = "Username"
-            addSubview(usernameDisplay)
-            usernameField = UITextField(frame: CGRect(x: 10, y: 153, width: frame.width-40, height: 60))
-            usernameField.placeholder = "OK"
-            addSubview(usernameField)
-            
-            emailDisplay = UILabel(frame: CGRect(x: 10, y: 228, width: frame.width-40, height: 30))
-            emailDisplay.text = "Email"
-            addSubview(emailDisplay)
-            
-            emailField = UITextField(frame: CGRect(x: 10, y: 263, width: frame.width-40, height: 60))
-            emailField.placeholder = "HAAHHA"
-            addSubview(emailField)
-        }
-    }
-
 }
-
