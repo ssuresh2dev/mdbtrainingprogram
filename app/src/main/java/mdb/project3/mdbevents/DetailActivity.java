9415dfcfe77eaceb11bde89b988aa675042016b7
Sayan Paul <sayan.paul.us@gmail.com>
2016-10-14 15:45:23 -0700

    Add AsyncTask for image loading

--- app/src/main/java/mdb/project3/mdbevents/DetailActivity.java
+++ app/src/main/java/mdb/project3/mdbevents/DetailActivity.java
@@ -1,11 +1,12 @@
 package mdb.project3.mdbevents;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,7 +18,6 @@ import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,10 +25,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Locale;
-import java.util.Map;
 
 
 public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
@@ -66,10 +67,11 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                Event event = snapshot.getValue(Event.class);
+                final Event event = snapshot.getValue(Event.class);
                 socialTitleView.setText(event.getName());
                 dateTextView.setText(event.date);
-                Glide.with(getApplicationContext()).load(event.imageUrl).into(eventImageView);
+                AsyncTask<Void, Void, Bitmap> getBitmap = createDownloadTask(event.imageUrl);
+                getBitmap.execute();
                 descriptionTextView.setText(event.description);
                 interestedButton.setText(String.format(Locale.getDefault(), "%d people interested", event.numInterested));
             }
@@ -92,6 +94,33 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 
         interestedButton.setOnClickListener(this);
         interestedToggleButton.setOnClickListener(this);
+    }
+
+    private AsyncTask<Void, Void, Bitmap> createDownloadTask(final String imageUrl) {
+        return new AsyncTask<Void, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(Void... params) {
+                try {
+                    URL url = new URL(imageUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoInput(true);
+                    connection.connect();
+                    InputStream input = connection.getInputStream();
+                    return BitmapFactory.decodeStream(input);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                if (bitmap != null) {
+                    eventImageView.setImageBitmap(bitmap);
+                }
+                super.onPostExecute(bitmap);
+            }
+        };
     }
 
     @Override
