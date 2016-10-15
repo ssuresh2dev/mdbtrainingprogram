6d246939999eb0820f2ffaf07c18ba5503f307b0
Rochelle Shen <rochelleshen@berkeley.edu>
2016-10-15 16:22:15 -0700

    API file

---
+++ Weather/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  Weather
+//
+//  Created by Rochelle on 10/15/16.
+//  Copyright © 2016 Rochelle. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class API {
+    
+}
