4780f2dc5fed659b30f8077523b2566123e1e007
Shubham Goenka <gshubham@berkeley.edu>
2016-10-15 17:48:31 -0700

    Added location label part 2

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -24,14 +24,16 @@ class ViewController: UIViewController {
         let locationLabel = UILabel(frame: CGRect(x: view.frame.width * 0.2, y: 40, width: view.frame.width * 0.6, height: 40))
         locationLabel.text = "Location"
         locationLabel.font = UIFont(name: "AvenirNext-Regular", size: 36.0)
-        locationLabel.textColor = UIColor(colorLiteralRed: 0, green: 204, blue: 204, alpha: 1)
+        locationLabel.textColor = UIColor(colorLiteralRed: 0, green: 204/255, blue: 204/255, alpha: 1)
         locationLabel.textAlignment = NSTextAlignment.center
         locationLabel.numberOfLines = 1
         locationLabel.adjustsFontSizeToFitWidth = true
         locationLabel.clipsToBounds = true
         view.addSubview(locationLabel)
         
-        let container = UIImageView(frame: CGRect(x: view.frame.width * 0.15, y: 100, width: view.frame.width * 0.7, height: view.frame.height * 0.5))
+        let container = UIImageView(frame: CGRect(x: view.frame.width * 0.15, y: 110, width: view.frame.width * 0.7, height: view.frame.height * 0.4))
+        container.image = #imageLiteral(resourceName: "clearBox")
+        view.addSubview(container)
     }
 
     override func didReceiveMemoryWarning() {
